f24bac83ff0000bdb3746cf3bca806e998753c36
Charles McQuain <charlesmcquain@gmail.com>
2020-09-21 20:56:47 -0700

    Created Footer and Layout components, gatsby-browser file

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 export default function OrderPage() {
   return (
     <div>
-      <Nav />
       <p>Hey! I'm the Order page.</p>
     </div>
   );
